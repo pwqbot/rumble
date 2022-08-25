3478fa9675074d28cf979c3918a1914a8c58a963
pwqbot <ding00000804@gmail.com>
2022-08-25 13:52:03 +0800

    done: concept in c++17

---
+++ cpp/concept_17/main.cpp
@@ -0,0 +1,96 @@
+#include <type_traits>
+
+namespace intro {
+
+template <typename T, typename vvv = void>
+struct has_foo : std::false_type {};
+
+template <typename T>
+struct has_foo<T, std::void_t<decltype(std::declval<T>().foo())>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<has_foo<T>::value>>
+auto CallFoo(T t) {
+    t.foo();
+}
+
+int main() {
+
+    int a;
+    /* CallFoo(a); */
+    struct fo {
+        void foo() {}
+    };
+    fo wd;
+    CallFoo(wd);
+
+    return 0;
+}
+
+} // namespace intro
+
+namespace concepts {
+
+struct memory_block {};
+
+template <typename T, template <typename...> class Expression,
+          typename vvv = void>
+struct compiles : std::false_type {};
+
+template <typename T, template <typename...> class Expression>
+struct compiles<T, Expression, std::void_t<Expression<T>>> : std::true_type {};
+
+template <typename T, template <typename...> class Expression,
+          typename ReturnType, typename vvv = void>
+struct compiles_same_type
+    : std::conjunction<std::is_same<ReturnType, Expression<T>>,
+                       compiles<T, Expression>> {};
+
+template <typename T, template <typename...> class Expression,
+          typename ReturnType, typename vvv = void>
+struct compiles_convertiable_type
+    : std::conjunction<std::is_convertible<ReturnType, Expression<T>>,
+                       compiles<T, Expression>> {};
+
+template <typename T>
+struct BlockAllocator_impl {
+    template <typename Allocator>
+    using allocate_block = decltype(std::declval<Allocator>().allocate_block());
+
+    template <typename Allocator>
+    using deallocate_block =
+        decltype(std::declval<Allocator>().deallocate_block(
+            std::declval<memory_block>()));
+
+    template <typename Allocator>
+    using next_block_size =
+        decltype(std::declval<const Allocator>().next_block_size());
+
+    using result =
+        std::conjunction<compiles<T, deallocate_block>,
+                         compiles_convertiable_type<T, next_block_size, size_t>,
+                         compiles_same_type<T, allocate_block, memory_block>>;
+};
+
+template <typename T>
+using BlockAllocator = typename BlockAllocator_impl<T>::result;
+
+template <typename ResultType, typename CheckType,
+          template <typename> typename... Concepts>
+using REQUIRE =
+    std::enable_if_t<std::conjunction<Concepts<CheckType>...>::value,
+                     ResultType>;
+
+template <typename T>
+auto alloc() -> REQUIRE<void, T, BlockAllocator> {}
+
+void test_alloc() {
+    struct wd {
+        auto next_block_size() const -> int;
+        auto deallocate_block(memory_block) -> void;
+        auto allocate_block() -> memory_block;
+    };
+    alloc<wd>();
+}
+
+} // namespace concepts
